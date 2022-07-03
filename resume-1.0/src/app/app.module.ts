ac154259003bfbda4bb1f49c4b8bad3d5c9ac72d
Zain <47285447+ZainMalik00@users.noreply.github.com>
2022-07-03 15:48:34 -0400

    Added Page Components and Routed Navbar

--- resume-1.0/src/app/app.module.ts
+++ resume-1.0/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { ExperienceComponent } from './components/pages/experience/experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent,
+    ContactComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
