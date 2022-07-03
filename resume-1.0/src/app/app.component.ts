00f02b4d5b853802cdfb6c59d402c1605cf62968
Zain <47285447+ZainMalik00@users.noreply.github.com>
2022-07-03 19:28:56 -0400

    Updated Smooth Scrolling and Dynamic Nav Logic

--- resume-1.0/src/app/app.component.ts
+++ resume-1.0/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'resume-1.0';
+  @ViewChild("aboutSection") aboutSection !: ElementRef;
+  @ViewChild("experienceSection") experienceSection !: ElementRef;
+  @ViewChild("contactSection") contactSection !: ElementRef;
+  
+  ngAfterViewInit() {
+    console.log('on after view init', this.aboutSection);
+  }
+
+  scrollToSection(selectedDiv : any): void {
+    switch (selectedDiv){
+      case "about":
+        this.aboutSection.nativeElement.scrollIntoView({behavior: "smooth"} ); //enables smooth scrolling
+        break;
+      case "experience":
+        this.experienceSection.nativeElement.scrollIntoView({behavior: "smooth"} ); //enables smooth scrolling
+        break;
+      case "contact":
+        this.contactSection.nativeElement.scrollIntoView({behavior: "smooth"} ); //enables smooth scrolling
+        break;
+    }
+  }
 }
--- resume-1.0/src/app/components/navbar/navbar.component.ts
+++ resume-1.0/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -8,14 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   constructor() { }
+  
+  @Output() navbarClick = new EventEmitter();
 
   ngOnInit(): void {}
 
-  clickedSection(selectedDiv : string): void {
-    const element = document.getElementById(selectedDiv);
-    if(element != null) {
-      element.scrollIntoView({behavior: "smooth"} );
-    }
+  clickedSection(section : string): void {
+    this.navbarClick.emit(section);
   }
 
+  @HostListener('window:resize', ['$event']) navbarToggle(event: any) : void {
+    const navList = document.getElementById("navbar-list");
+    const navListContent = document.getElementById("navbar-list-content");
+
+    if(navList != null && navListContent != null) {//null check
+      if(navListContent.offsetWidth > navList.offsetWidth){//when navbar list overflows to the left side
+
+      }
+    }
+  }
 }
